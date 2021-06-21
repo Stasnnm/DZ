dad4190dedd2557c6bd635f5d2c18c051017e1c8
Stasnnm <85891030+Stasnnm@users.noreply.github.com>
2021-06-21 13:20:49 +0300

    Add files via upload

---
+++ dz1.py
@@ -0,0 +1,15 @@
+from art import *
+tprint('Stas')
+print('vvedite x')
+x = int(input())
+print('vvedite y')
+y = int(input())
+print('x=', x, 'y=', y)
+s = x + y
+print('summa', s)
+d = x // y
+print('delenie', d)
+ost = x % y
+print('ostatok', ost)
+step = x ** y
+print ('stepen', step)
